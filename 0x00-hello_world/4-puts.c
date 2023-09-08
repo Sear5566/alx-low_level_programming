36b1a3873f4929ffe2ea0b1c9448059ff2680a7c
Sear5566 <dwdsegun63@gmail.com>
2023-09-08 02:44:25 +0100

    Using puts function + betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - this is the main entry point (function)
+* puts - prints the string on the console
+* return - would always be 0, i.e success
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
