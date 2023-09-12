0df1b9884a4dd8df380dbfe623c25f7916681320
Sear5566 <dwdsegun63@gmail.com>
2023-09-12 14:32:04 +0100

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * print_sign - prints the sign of a number
+  * @n: the number to be printed
+  * Return: 1 and prints + if number is greater than zero,
+  * 0 and prints 0 if number is zero,
+  * -1 and prints - if number is less than zero.
+*/
+int print_sign(int n)
+{
+	if (n > 48)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 48)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
