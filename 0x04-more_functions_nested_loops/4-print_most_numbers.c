3fc29fa47911298a3ded0ec617d1faef87887aae
Sear5566 <dwdsegun63@gmail.com>
2023-09-14 23:51:08 +0100

    more numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * print_most_numbers - prints numbers from 0 to 9
+  * except 2 and 4
+*/
+void print_most_numbers(void)
+{
+	int numb;
+
+	for (numb = '0'; numb <= '9'; numb++)
+	{
+		if (numb != '2' && numb != '4')
+		{
+			_putchar(numb);
+		}
+		_putchar('\n');
+	}
+}
