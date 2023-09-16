44d1f2146fb4c22b313b4dbc9e40059b48238bc9
Sear5566 <dwdsegun63@gmail.com>
2023-09-16 02:20:41 +0100

    my main positif

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,16 +6,16 @@
   */
 void positive_or_negative(int i)
 {
-	if (i < 0)
+	if (i > 0)
 	{
-		printf("%d is %s\n", i, "negative");
+		printf("%d is positive\n", i);
 	}
-	else if (i > 0)
+	else if (i < 0)
 	{
-		printf("%d is %s\n", i, "positive");
+		printf("%d is negative\n", i);
 	}
 	else
 	{
-		printf("%d is %s\n", i, "zero");
+		printf("%d is zero\n", i);
 	}
 }
