8e790817916453303b2471817fc9cbfa9dd58e4f
Sear5566 <dwdsegun63@gmail.com>
2023-09-21 23:42:17 +0100

    string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string into 1337
+ * @str: character to string pointer
+ * Return: pointer to 'str'
+ */
+char *leet(char *str)
+{
+	int sl, c;
+	char leetalph[] = "aAeEoOtTlL";
+	char leetNumb[] = "4433007711";
+
+	sl = 0;
+	while (str[sl] != '\n')
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (leetalph[c] == str[sl])
+			{
+				str[sl] = leetNumb[c];
+			}
+			c++;
+		}
+		sl++;
+	}
+
+	return (str);
+}
