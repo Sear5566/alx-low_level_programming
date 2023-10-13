291cfb99188f37bed29d01203766fb32a36b94fc
Sear5566 <dwdsegun63@gmail.com>
2023-10-13 06:49:43 +0100

    printing all strings

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+  * print_all - a function that prints strings, followed by a new line.
+  * @format: "cifs"
+*/
+void print_all(const char * const format, ...)
+{
+	va_list my_arg;
+	unsigned int i = 0;
+	char *S;
+
+	va_start(my_arg, format);
+	while (format && format[i])
+	{
+		switch (format[i++])
+		{
+			case 'c':
+				printf("%c", va_arg(my_arg, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(my_arg, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(my_arg, double));
+				break;
+			case 's':
+				S = va_arg(my_arg, char*);
+				if (S == NULL)
+				{
+					S = "(nil)";
+				}
+				printf("%s", S);
+				break;
+			default:
+				continue;
+		}
+		if (format[i])
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	va_end(my_arg);
+}
